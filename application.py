124dc8c8f057f220c90456ecc3d6c9360683c84c
bfortuner <bfortuner@gmail.com>
2015-10-18 12:53:06 -0700

    1) initialized EBS application

--- application.py
+++ application.py
@@ -1,12 +1,3 @@
-'''
-Simple Flask application to test deployment to Amazon Web Services
-Uses Elastic Beanstalk and RDS
-
-Author: Scott Rodkey - rodkeyscott@gmail.com
-
-Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
-'''
-
 from flask import Flask, render_template, request
 from application import db
 from application.models import Data
@@ -16,7 +7,7 @@ from application.forms import EnterDBInfo, RetrieveDBInfo
 application = Flask(__name__)
 application.debug=True
 # change this to your own value
-application.secret_key = 'cC1YCIWOj9GgWspgNEo2'   
+application.secret_key = 'wazzupman'   
 
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
