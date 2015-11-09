4cfa0b57817a331c0301aab5984a0e4fd4f728b8
Brendan Fortuner <fortuner@amazon.com>
2015-11-09 15:39:57 -0800

    Added job_status to email_confirmation email

--- bigneuron_app/emails/constants.py
+++ bigneuron_app/emails/constants.py
@@ -2,25 +2,30 @@ from bigneuron_app.clients.constants import AWS_IAM_USER_LOGIN_LINK
 
 ADMIN_EMAIL='vaa3dapi@gmail.com'
 
+HEADER="<html>"
+FOOTER="</html>"
 CREATE_JOB_CONFIRMATION_TEMPLATE="""<p>Hello,</p>
 <p>Your new Vaa3D job is running! You will receive a confirmation email when your job completes.</p>
 <p>Thanks,<br>
 Vaa3D Team</p>
 """
+CREATE_JOB_CONFIRMATION_TEMPLATE = HEADER + CREATE_JOB_CONFIRMATION_TEMPLATE + FOOTER
 
 COMPLETE_JOB_CONFIRMATION_TEMPLATE="""<p>Hello,</p>
 <p>Your new Vaa3D job is complete! Please log in to AWS S3 using the link below. You can download your output files from the vaa3d-output bucket</p>
+<p>Job Status: %s</p>
 <p><a href="%s">Login To AWS S3</a></p>
 <p>Thanks,<br>
 Vaa3D Team</p>
-""" % AWS_IAM_USER_LOGIN_LINK
+"""
+COMPLETE_JOB_CONFIRMATION_TEMPLATE = HEADER + COMPLETE_JOB_CONFIRMATION_TEMPLATE + FOOTER
 
 CREATE_JOB_CONFIRMATION ={
 	'subject' : 'Your New Vaa3D Job',
-	'body' : "<html>" + CREATE_JOB_CONFIRMATION_TEMPLATE + "</html>"
+	'body' : CREATE_JOB_CONFIRMATION_TEMPLATE
 }
 
 COMPLETE_JOB_CONFIRMATION ={
 	'subject' : 'Your Vaa3D Job is Complete!',
-	'body' : "<html>" + COMPLETE_JOB_CONFIRMATION_TEMPLATE + "</html>"
+	'body' : COMPLETE_JOB_CONFIRMATION_TEMPLATE
 }
--- bigneuron_app/emails/email_manager.py
+++ bigneuron_app/emails/email_manager.py
@@ -2,6 +2,7 @@ from bigneuron_app.emails.constants import *
 from bigneuron_app.users import user_manager
 from bigneuron_app.clients import ses
 from bigneuron_app.jobs.models import Job
+from bigneuron_app.clients.constants import AWS_IAM_USER_LOGIN_LINK
 
 
 def send_job_created_email(job):
@@ -11,10 +12,15 @@ def send_job_created_email(job):
 
 def send_job_complete_email(job):
 	user = user_manager.get_user_by_id(job.user_id)
+	body = get_job_complete_template(job)
 	ses.send_email(COMPLETE_JOB_CONFIRMATION['subject'],
-		COMPLETE_JOB_CONFIRMATION['body'], user.email)
+		body, user.email)
+
+def get_job_complete_template(job):
+	return COMPLETE_JOB_CONFIRMATION['body'] % (job.job_status.status_name, AWS_IAM_USER_LOGIN_LINK)
 
 def test_email_manager():
 	job = Job.query.first()
+	print get_job_complete_template(job)
 	send_job_created_email(job)
 	send_job_complete_email(job)
